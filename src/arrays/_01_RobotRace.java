b56727f0f8eb0e37a477827a8f48b1245a83a398
student <student@ip-172-31-53-24.us-west-2.compute.internal>
2021-09-27 16:56:02 -0700

    processing

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,35 +1,48 @@
 package arrays;
 
+import java.util.Random;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
-	//1. make a main method
-public static void main(String[] args) {
-	
+	// 1. make a main method
+	public static void main(String[] args) {
 
-	//2. create an array of 5 robots.
-Robot[] array = new Robot[5];
-	//3. use a for loop to initialize the robots.
+		// 2. create an array of 5 robots.
+		Robot[] array = new Robot[5];
+		// 3. use a for loop to initialize the robots.
+		Random ran = new Random();
 
-for(int i = 0;i < array.length ;i++) {
-	Robot e = new Robot();
-	e.setSpeed(10);
-	e.setX(50);
-	e.setY(450);
-	e.move(20);
-}
-		//4. make each robot start at the bottom of the screen, side by side, facing up
+		for (int i = 0; i < array.length; i++) {
+			array[i] = new Robot();
+			array[i].setSpeed(10);
+			array[i].setX(i * 120 + 70);
+			array[i].setY(450);
+		}
+		boolean isFinished = false;
+		while (!isFinished) {
+			for (int i = 0; i < array.length; i++) {
+				int ranum = ran.nextInt(50);
+				array[i].move(ranum);
+				if (array[i].getY() < 50) {
+					isFinished = true;
+					System.out.println("Robot "+i+" won! party time");
+					break;
+				}
+			}
+		}
+		// 4. make each robot start at the bottom of the screen, side by side, facing up
 
+		// 5. use another for loop to iterate through the array and make each robot move
+		// a random amount less than 50.
 
-	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
+		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+		// screen.
 
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+		// 7. declare that robot the winner and throw it a party!
 
-	//7. declare that robot the winner and throw it a party!  
+		// 8. try different races with different amounts of robots.
 
-	//8. try different races with different amounts of robots.
-
-	//9. make the robots race around a circular track.
-}
+		// 9. make the robots race around a circular track.
+	}
 }
--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -9,63 +9,87 @@ package arrays;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
-	
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class _02_FindTheHiddenButton implements ActionListener{
+public class _02_FindTheHiddenButton implements ActionListener {
 	JFrame window;
 	JPanel panel;
-	
-	//1. create an array of JButtons. Don't initialize it yet.
-	
-	//2 create an int variable called hiddenButton
-	
+
+	// 1. create an array of JButtons. Don't initialize it yet.
+	JButton[] array;
+	// 2 create an int variable called hiddenButton
+	int hiddenButton;
+
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
-	
+
 	public void start() {
 		window = new JFrame("Find the Button");
 		panel = new JPanel();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//3.  Ask the user to enter a positive number and convert it to an int
-		
-		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		
-		//5. Make a for loop to iterate through the JButton array
-			//6. initialize each JButton in the array
-			//7. add the ActionListener to each JButton
-			//8. add each JButton to the panel
-		
-		//9 add the panel to the window
-		
-		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
-		
-		//11. set the JFrame to visible.
-		
-		//12. Give the user the instructions for the game.
-		
-		//13. initialize the hiddenButton variable to a random number less than the int created in step 3
-		
-		//14. Set the text of the JButton located at hiddenButton to  "ME"
 
-		//15. Use Thread.sleep(1000); to pause the program.
-		//    Surround it with a try/catch - use Eclipse helper for this
+		// 3. Ask the user to enter a positive number and convert it to an int
+		String input = JOptionPane.showInputDialog("Enter a positve number kid");
+		int answer = Integer.parseInt(input);
+		// 4. Initialize the array of JButtons to be the size of the int created in step
+		// 3
+		array = new JButton[answer];
+		// 5. Make a for loop to iterate through the JButton array
+		// 6. initialize each JButton in the array
+		// 7. add the ActionListener to each JButton
+		// 8. add each JButton to the panel
+		Random ran = new Random();
+		hiddenButton = ran.nextInt(answer);
+		for (int i = 0; i < array.length; i++) {
+			array[i] = new JButton();
+			array[i].addActionListener(this);
+			panel.add(array[i]);
+		}
+		array[hiddenButton].setText("Me");
+		// 9 add the panel to the window
+		window.add(panel);
+		// 10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		// 11. set the JFrame to visible.
+		window.setVisible(true);
+		// 12. Give the user the instructions for the game.
+		JOptionPane.showMessageDialog(null, "I don't even know the instructions on this game so your on ur own kid");
+		// 13. initialize the hiddenButton variable to a random number less than the int
+		// created in step 3
 		
-		//16. Set the text of the JButton located at hiddenButton to be blank.
+		// 14. Set the text of the JButton located at hiddenButton to "ME"
 		
+	
+		// 15. Use Thread.sleep(1000); to pause the program.
+		// Surround it with a try/catch - use Eclipse helper for this
+try {
+	Thread.sleep(1000);
+	array[hiddenButton].setText("");
+} catch (InterruptedException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+	
+}
+		// 16. Set the text of the JButton located at hiddenButton to be blank.
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		JButton buttonClicked = (JButton)e.getSource();
-		
-		//17. if the hiddenButton is clicked, tell the user that they win.
-		
-		//18. else tell them to try again
+		JButton buttonClicked = (JButton) e.getSource();
+
+		// 17. if the hiddenButton is clicked, tell the user that they win.
+if(array[hiddenButton]== buttonClicked ) {
+	JOptionPane.showMessageDialog(null, "you won");
+}
+else {
+	JOptionPane.showMessageDialog(null, "you lost");
+}
+		// 18. else tell them to try again
 	}
 }
